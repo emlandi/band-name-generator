ed7aa90b8de64a2c2f6049f08998b01753c0032c
Emily Landi <emily.landi@gmail.com>
2015-09-30 21:08:03 -0700

    add app folder with html and js files

---
+++ app/script.js
@@ -0,0 +1,6 @@
+$(function() {
+  $("button").click(function() {
+    $("#adjective").text("hello world");
+  });
+
+});
--- server.js
+++ server.js
@@ -3,21 +3,22 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/app/"));
+
+app.listen(port, function() {
+  console.log("server started on port " + port);
+});
+
+app.get("/", function (req, res) {
+  // res.send("hello universe!");
+  res.sendFile(index.html);
+});
+
 var Adjective = function() {
   this.tall = true;
   this.thirsty = true;
   this.happy = true;
 }
-
-app.listen(port, function() {
-  console.log("server started on port " + port);
-  console.log(getRandomWord(adjective));
-});
-
-app.get("/", function (req, res) {
-  res.send("hello universe!");
-});
-
 
 var adjective = new Adjective();
 
@@ -28,7 +29,6 @@ function getRandomWord(object) {
 }
 
 getRandomWord(adjective);
-
 
 app.get("/adjective", function (req, res) {
   res.json(getRandomWord(adjective));
